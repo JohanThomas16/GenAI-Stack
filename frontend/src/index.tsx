177eb9387253bf7714297601a3436f35c509613e
JohanThomas16 <169023658+JohanThomas16@users.noreply.github.com>
2025-09-09 00:41:50 +0530

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
-
+html, body, #root {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
